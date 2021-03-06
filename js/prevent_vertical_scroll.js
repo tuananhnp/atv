db4ac392e90996941c380bbf8f0b900986b50818
tuananhnp <tuananhnp1001@gmail.com>
2021-03-07 00:18:10 +0100

    Update prevent_vertical_scroll.js

--- js/prevent_vertical_scroll.js
+++ js/prevent_vertical_scroll.js
@@ -1,26 +1,18 @@
-var $j = jQuery.noConflict();
-$j( document ).ready( function() {
-    var owl = $j( ".owl-carousel" ).owlCarousel( {
-        onDragged: function() {
-            $j( 'body' ).css( 'overflow', 'auto' );
-        },
-        onDrag: function() {
-            $j( 'body' ).css( 'overflow', 'hidden' );
-        },
-    } );
+var owl = $('.owl-carousel');
+    owl.owlCarousel({
+    //  your options
+})
 
-    // disable scroll
-    owl.on( 'drag.owl.carousel', function( event ) {
-        document.ontouchmove = function( e ) {
-            e.preventDefault();
-        }
-    } );
+// disable scroll
+owl.on('drag.owl.carousel', function(event) {
+    document.ontouchmove = function (e) {
+        e.preventDefault()
+    }
+})
 
-    // enable scroll
-    owl.on( 'dragged.owl.carousel', function( event ) {
-        document.ontouchmove = function( e ) {
-            return true;
-        }
-    } );
-
-} );
+// enable scroll
+owl.on('dragged.owl.carousel', function(event) {
+    document.ontouchmove = function (e) {
+        return true
+    }
+})
