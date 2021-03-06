b16481d9bc89dac7d874015faccf5e26e1686821
tuananhnp <tuananhnp1001@gmail.com>
2021-03-07 00:15:37 +0100

    Update prevent_vertical_scroll.js

--- js/prevent_vertical_scroll.js
+++ js/prevent_vertical_scroll.js
@@ -1,29 +1,12 @@
 var $j = jQuery.noConflict();
 $j( document ).ready( function() {
     var owl = $j( ".owl-carousel" ).owlCarousel( {
-        stagePadding: 90,
-        loop: true,
-        lazyLoad: true,
-        dots: false,
-        margin: 10,
-        nav: false,
         onDragged: function() {
             $j( 'body' ).css( 'overflow', 'auto' );
         },
         onDrag: function() {
             $j( 'body' ).css( 'overflow', 'hidden' );
         },
-        responsive: {
-            0: {
-                items: 2
-            },
-            500: {
-                items: 4
-            },
-            1000: {
-                items: 5
-            }
-        }
     } );
 
     // disable scroll
