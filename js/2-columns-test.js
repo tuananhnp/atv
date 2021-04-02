7a12d0d7dea856d357fc3d268ab979e812e9aaa6
tuananhnp <tuananhnp1001@gmail.com>
2021-04-02 22:55:59 +0200

    new

--- js/2-columns-test.js
+++ js/2-columns-test.js
@@ -1,31 +1,45 @@
-// find the top of each section
-    var section1 = $('#starrynight-section').offset().top;
-    var section2 = $('#moomin-front-section').offset().top;
-    // number of pixels before the section to change image
-    var scrollOffset = 500;
-    // run this function when the window scrolls
-    $(window).scroll(function() {
-      // get the window height on scroll
-      var scroll = $(window).scrollTop() + scrollOffset;
-      // if scroll hits the top of section 1  
-      if (scroll < 2500) {
-        $('.svg-title-desktop-starrynight').fadeIn(function() {
-          $(this).addClass('inactive');
-        });
-        $('.svg-title-desktop-starrynight').fadeIn(function() {
-          $(this).addClass('active');
-        });
-        $('.svg-title-desktop-moomin-front').fadeIn(function() {
-          $(this).removeClass('active');
-        });
-      }
-      // if scroll hits the top of section 2
-      if (scroll > section2) {
-        $('.svg-title-desktop-starrynight').fadeIn(function() {
-          $(this).removeClass('active');
-        });
-        $('.svg-title-desktop-moomin-front').fadeIn(function() {
-          $(this).addClass('active');
-        });
-      }
-    });
+var scene1 = $('#starrynight-front-scene').offset().top;
+var scene2 = $('#starrynight-back-scene').offset().top;
+var scene3 = $('#moomin-front-scene').offset().top;
+
+var scrollOffset = 300;
+
+$(window).scroll(function() {
+  var scroll = $(window).scrollTop() + scrollOffset;
+  if (scroll < 2000) {
+	$('.svg-title-desktop-starrynight-front').fadeIn(function() {
+	  $(this).addClass('inactive');
+	});
+	$('.svg-title-desktop-starrynight-front').fadeIn(function() {
+	  $(this).addClass('active');
+	});
+	$('.svg-title-desktop-starrynight-back').fadeIn(function() {
+	  $(this).removeClass('active');
+	});
+  }
+  if (scroll > scene2) {
+	$('.svg-title-desktop-starrynight-front').fadeIn(function() {
+	  $(this).addClass('inactive');
+	});
+	$('.svg-title-desktop-starrynight-front').fadeIn(function() {
+	  $(this).removeClass('active');
+	});
+	$('.svg-title-desktop-starrynight-back').fadeIn(function() {
+	  $(this).addClass('active');
+	});
+	$('.svg-title-desktop-moomin-front').fadeIn(function() {
+	  $(this).removeClass('active');
+	});
+  }
+  if (scroll > scene3) {
+	$('.svg-title-desktop-starrynight-front').fadeIn(function() {
+	  $(this).addClass('inactive');
+	});
+	$('.svg-title-desktop-starrynight-back').fadeIn(function() {
+	  $(this).removeClass('active');
+	});
+	$('.svg-title-desktop-moomin-front').fadeIn(function() {
+	  $(this).addClass('active');
+	});
+  }
+});
